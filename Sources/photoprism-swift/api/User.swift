966753b78cf9c1da65afa35964b5bee6cd2c4112
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-02-06 17:08:48 +0900

    new: Session.swift, User.swift

---
+++ Sources/photoprism-swift/api/User.swift
@@ -0,0 +1,12 @@
+/**
+ * @author petitstrawberry
+ * @email petitstrawberry.dev@gmail.com
+ * @create date 2023-02-06 15:09:01
+ * @modify date 2023-02-06 15:12:05
+ * @desc User
+ */
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
--- Sources/photoprism-swift/photoprism_swift.swift
+++ Sources/photoprism-swift/photoprism_swift.swift
@@ -2,5 +2,8 @@ public struct photoprism_swift {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print("Hello")
+
+        let session = Session.getSession(host: "192.168.0.38:20800", isSSL: false, user: User(username: "admin", password: "photo-mariko-broken"))
     }
 }
