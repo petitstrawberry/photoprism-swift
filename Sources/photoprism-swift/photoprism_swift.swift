2f9667b9376a881d0243d23ca2d9331b22ff736b
PetitStrawberry <deloreantaro@yahoo.co.jp>
2023-02-06 14:58:55 +0900

    initialize swift package

---
+++ Sources/photoprism-swift/photoprism_swift.swift
@@ -0,0 +1,6 @@
+public struct photoprism_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/photoprism-swiftTests/photoprism_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import photoprism_swift
+
+final class photoprism_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(photoprism_swift().text, "Hello, World!")
+    }
+}
